3579e610cb5dd456db5cddf60dd8d453e2ace645
Azharul Islam <azharulip@gmail.com>
2023-05-19 14:47:23 +0600

    hero section design done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,8 +3,17 @@
 
 const Home = () => {
     return (
-        <div>
-            <p className="text-7xl text-right">hello</p>
+        <div className="py-5">
+            <div className="container mx-auto grid grid-cols-3 justify-center items-center text-center p-5 border border-slate-600 shadow shadow-lg shadow-slate-600">
+                <div className="col-span-2">
+                    <img src="https://images.pexels.com/photos/6132059/pexels-photo-6132059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+
+                </div>
+                <div className="">
+                    <p className="text-3xl text-white">Visit our letest Car Toys</p>
+                   <button className="btn bg-slate-600 text-white">Visit</button>
+                </div>
+            </div>
         </div>
     );
 };
