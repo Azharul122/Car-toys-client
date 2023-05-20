b05dbdb4f27eabdb56f58731ea8f3706cbe11938
Azharul Islam <azharulip@gmail.com>
2023-05-20 23:46:16 +0600

    Toy deleted done

--- src/Components/toys/My_toy.jsx
+++ src/Components/toys/My_toy.jsx
@@ -3,22 +3,54 @@ import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit,faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faEdit,faTrash } from '@fortawesome/free-solid-svg-icons'
+import Swal from 'sweetalert2'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const My_toy = () => {
-  const [username, setuserName] = useState(null);
 
   const { user } = useContext(AuthContext);
-  const toys = useLoaderData();
+  const [toys,setToys] = useState(useLoaderData());
+  const {_id}=toys
 
-  if (user) {
-    // setuserName(user.displayName)
-  }
+const handleUpdate=(event)=>{
 
-  // console.log(toy)
+}
+
+const handleDelete=(_id)=>{
+console.log(_id)
+Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes, delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+   
+        fetch(`http://localhost:5000/toy/${_id}`,{
+            method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            if(data.deletedCount>0)
+            {
+               toast("Success Fully Deleted")
+                  setToys(useLoaderData())
+            }
+        })
+    }
+  })
+
+}
 
   return (
     <div>
+        <ToastContainer></ToastContainer>
       <div className="container mx-auto py-5">
         <div className="overflow-x-auto">
           <table className="table w-full">
@@ -52,8 +84,8 @@ const My_toy = () => {
                     <td>{t.sellerMail}</td>
                     <td><img src={t.photoURL} alt="" className="w-[40px] h-[40px]" /></td>
                     <td>{t.description}</td>
-                    <td><FontAwesomeIcon icon={faEdit}  className="text-green-600 mr-2 cursor-pointer"/>
-                    <FontAwesomeIcon icon={faDeleteLeft}  className="text-red-600 cursor-pointer"/>
+                    <td><FontAwesomeIcon onClick={handleUpdate} icon={faEdit}  className="text-green-600 mr-2 cursor-pointer"/>
+                    <FontAwesomeIcon onClick={()=> handleDelete(t._id)} icon={faTrash}  className="text-red-600 cursor-pointer"/>
                     </td>
                    
                   </tr>
