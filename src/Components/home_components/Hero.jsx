ee0eab3e1d51349b1a4df40e463b9a071ff9ceb7
Azharul Islam <azharulip@gmail.com>
2023-05-21 15:47:13 +0600

    AOS animation effect added done on Home Page

--- src/Components/home_components/Hero.jsx
+++ src/Components/home_components/Hero.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 const Hero = () => {
     Aos.init();
     return (
-        <div>
+        <div data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000">
              <div  className="container mx-auto grid grid-cols-3 justify-center items-center text-center p-5 border border-slate-600 shadow shadow-lg shadow-slate-600" data-aos="flip-left">
                 <div className="col-span-2">
                     <img src="https://images.pexels.com/photos/6132059/pexels-photo-6132059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
